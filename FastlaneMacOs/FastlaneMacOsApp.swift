44b53970410f9cb58940e38ae2bd2e4a7be3fb2c
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-10-02 00:10:59 -0400

    Initial Commit

---
+++ FastlaneMacOs/FastlaneMacOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneMacOsApp.swift
+//  FastlaneMacOs
+//
+//  Created by Gaurav Bhambhani on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneMacOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FastlaneTutorial/FastlaneTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneTutorialApp.swift
+//  FastlaneTutorial
+//
+//  Created by Gaurav Bhambhani on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
